dead2bbeaafcccc6cd6b69b9a096f22158dcd9fc
KEVIN_ARIEL_CARBAJAL_ESTRADA <a.22308051281052@cbtis128.edu.mx>
2025-04-30 14:21:29 +0000

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,153 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(FSN());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class FSN extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LoginScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
+class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                Container(
+                  color: Colors.blueGrey,
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(Icons.person_outline, size: 30),
+                      Row(
+                        children: [
+                          Icon(Icons.home_outlined, size: 30),
+                          SizedBox(width: 15),
+                          Icon(Icons.menu, size: 30),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.center,
+                    color: Color(0xFFBA4A00), // naranja oscuro
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "10% DE DESCUENTO",
+                          style: TextStyle(
+                              fontStyle: FontStyle.italic, fontSize: 20),
+                        ),
+                        SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: List.generate(
+                            5,
+                            (index) => Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 4),
+                              child: Icon(Icons.circle, size: 8),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Center(
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 30),
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(color: Colors.lightBlue, width: 2),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text("INICIO DE SESION",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold)),
+                    SizedBox(height: 20),
+                    TextField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.email_outlined),
+                        labelText: "Correo",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.key_outlined),
+                        labelText: "Contraseña",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Text("¿Olvidaste tu contraseña?",
+                            style: TextStyle(fontSize: 12)),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: Colors.white,
+                          radius: 25,
+                          child: IconButton(
+                            icon: Icon(Icons.close, color: Colors.lightBlue),
+                            onPressed: () {},
+                          ),
+                        ),
+                        SizedBox(width: 30),
+                        CircleAvatar(
+                          backgroundColor: Colors.white,
+                          radius: 25,
+                          child: IconButton(
+                            icon: Icon(Icons.check, color: Colors.lightBlue),
+                            onPressed: () {},
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 15),
+                    Text("¿No tienes cuenta?"),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text("regístrate",
+                          style: TextStyle(color: Colors.lightBlue)),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
